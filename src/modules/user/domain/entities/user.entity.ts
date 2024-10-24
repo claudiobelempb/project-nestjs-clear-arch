1e0e7fedecb9afbfcd1cc16f6a25e423f525dc24
claudiobelempb <claudio.c.lima@hoymail.com>
2024-10-24 07:51:29 -0300

    13. Criação da entidade User

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class UserEntiry {}
+export type UserPros = {
+  name: string
+  email: string
+  password: string
+  active?: boolean
+  createdAt?: Date
+  updatedAt?: Date
+}
+export class UserEntiry {
+  constructor(private readonly props: UserPros) {
+    ;(this.props.active = this.props.active ?? true),
+      (this.props.createdAt = this.props.createdAt ?? new Date())
+    this.props.updatedAt = this.props.createdAt ?? new Date()
+  }
+}
