99ab2bb873202c81337528bba6e6d584373f53ab
claudiobelempb <claudio.c.lima@hoymail.com>
2024-10-28 08:54:24 -0300

    49. Testes unitários de InMemoryRepository - método update

--- src/shared/domain/repositories/__tests__/unit/in-memory-repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-repository.spec.ts
@@ -41,4 +41,22 @@ describe('InMemoryRepository unit tests', () => {
     const result = await sut.findAll()
     expect([entity]).toStrictEqual(result)
   })
+
+  it('Should throw error on update when entity not found', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 })
+    await expect(sut.update(entity)).rejects.toThrow(
+      new EntityNotFoundError('Entity not found'),
+    )
+  })
+
+  it('Should update an entity', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 })
+    await sut.insert(entity)
+    const entityUpdate = new StubEntity(
+      { name: 'update name', price: 10 },
+      entity._id,
+    )
+    await sut.update(entityUpdate)
+    expect(entityUpdate.toJSON()).toStrictEqual(sut.itens[0].toJSON())
+  })
 })
