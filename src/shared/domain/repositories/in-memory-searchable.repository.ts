83adb514d1ad7a4d9a08265175e082cdebcb1bdc
claudiobelempb <claudio.c.lima@hoymail.com>
2024-10-29 13:51:47 -0300

    59. Implementação do método search no repositório InMemory - parte 1

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -1,12 +1,31 @@
 import { AppEntity } from '../entities/app-entity'
 import { InMemoryRepository } from './in-memory.repository'
 import { SeachacleRepositoryInteface } from './searchable-repository-contracts'
+import { SearchParams } from './utils/search-params'
+import { SearchResult } from './utils/search-result'
 
 export abstract class InMemorySearchableRepository<E extends AppEntity>
   extends InMemoryRepository<E>
   implements SeachacleRepositoryInteface<E, any, any>
 {
-  search(props: any): Promise<any> {
+  async search(props: SearchParams): Promise<SearchResult<E>> {
     throw new Error('Method not implemented.')
   }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {}
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {}
 }
