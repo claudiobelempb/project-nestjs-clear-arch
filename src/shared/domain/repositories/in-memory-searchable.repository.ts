0f08993d5332d32659a29dcf746a7088ed065891
claudiobelempb <claudio.c.lima@hoymail.com>
2024-10-29 14:11:18 -0300

    61. Implementação do método applySort no repositório InMemory

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -8,6 +8,7 @@ export abstract class InMemorySearchableRepository<E extends AppEntity>
   extends InMemoryRepository<E>
   implements SeachacleRepositoryInteface<E, any, any>
 {
+  sortableFields: string[] = []
   async search(props: SearchParams): Promise<SearchResult<E>> {
     const itemsFiltered = await this.applyFilter(this.items, props.filter)
     const itemsSorted = await this.applySort(
@@ -42,7 +43,22 @@ export abstract class InMemorySearchableRepository<E extends AppEntity>
     items: E[],
     sort: string | null,
     sortDir: string | null,
-  ): Promise<E[]> {}
+  ): Promise<E[]> {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items
+    }
+    return [...items].sort((a, b) => {
+      if (a.props[sort] < b.props[sort]) {
+        return sortDir === 'asc' ? -1 : 1
+      }
+
+      if (a.props[sort] > b.props[sort]) {
+        return sortDir === 'asc' ? 1 : -1
+      }
+
+      return 0
+    })
+  }
 
   protected async applyPaginate(
     items: E[],
