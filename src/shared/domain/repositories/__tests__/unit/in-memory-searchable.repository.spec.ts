d58856b3c17f43da8b7713363edd3d20e1ae3cbb
claudiobelempb <claudio.c.lima@hoymail.com>
2024-10-30 08:58:45 -0300

    64. Testes unitários do método applyFilter de InMemorySearchableRepository

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -1,5 +1,6 @@
 import { AppEntity } from '@/shared/domain/entities/app-entity'
 import { InMemorySearchableRepository } from '../../in-memory-searchable.repository'
+import { filter } from 'rxjs'
 
 type StubEntityProps = {
   name: string
@@ -30,7 +31,33 @@ describe('StubInMemorySearchableRepository unit tests', () => {
   })
 
   describe('applyFilter method', () => {
-    it('', () => {})
+    it('shoul no filter items when filter param is null', async () => {
+      const items = [new StubEntity({ name: 'name value', price: 50 })]
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+      const itemsFilter = await sut['applyFilter'](items, null)
+      expect(itemsFilter).toStrictEqual(items)
+      expect(spyFilterMethod).not.toHaveBeenCalled()
+    })
+
+    it('shoul filter using a filter param', async () => {
+      const items = [
+        new StubEntity({ name: 'test', price: 50 }),
+        new StubEntity({ name: 'TEST', price: 50 }),
+        new StubEntity({ name: 'fake', price: 50 }),
+      ]
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+      let itemsFilter = await sut['applyFilter'](items, 'TEST')
+      expect(itemsFilter).toStrictEqual([items[0], items[1]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1)
+
+      itemsFilter = await sut['applyFilter'](items, 'test')
+      expect(itemsFilter).toStrictEqual([items[0], items[1]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2)
+
+      itemsFilter = await sut['applyFilter'](items, 'no-filter')
+      expect(itemsFilter).toHaveLength(0)
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3)
+    })
   })
 
   describe('applySort method', () => {
