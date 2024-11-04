839a783e5e32ec6bf8f246fe9b090119d0b0a05d
claudiobelempb <claudio.c.lima@hoymail.com>
2024-11-04 14:15:37 -0300

    97. Testes unitários do caso de uso para excluir usuários

---
+++ src/modules/user/application/usecases/__tests__/unit/user-delete-usecase.spec.ts
@@ -0,0 +1,30 @@
+import { UserInMemoryRepository } from '@/modules/user/infra/database/in-memory/repositories/user-in-memory.repository'
+import { NotFoundError } from '@/shared/domain/errors/entity-not-found.error'
+import { UserEntiry } from '@/modules/user/domain/entities/user.entity'
+import { UserDataBuilder } from '@/modules/user/domain/testing/helper/user-data-builder'
+import { UserDeleteUseCase } from '../../user-delete.usecase'
+
+describe('UserDeleteUseCase unit tests', () => {
+  let sut: UserDeleteUseCase.UseCase
+  let repository: UserInMemoryRepository
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    sut = new UserDeleteUseCase.UseCase(repository)
+  })
+  it('Should throws error when entity not found', async () => {
+    await expect(() => sut.execute({ id: 'fakeId' })).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('Should delete a user', async () => {
+    const spyDelete = jest.spyOn(repository, 'delete')
+    const items = [new UserEntiry(UserDataBuilder({}))]
+    repository.items = items
+    expect(repository.items).toHaveLength(1)
+    await sut.execute({ id: items[0].id })
+    expect(spyDelete).toHaveBeenCalledTimes(1)
+    expect(repository.items).toHaveLength(0)
+  })
+})
