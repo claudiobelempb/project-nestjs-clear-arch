c3f4aded386b481337d7638a477d99a9d3b5ab63
claudiobelempb <claudio.c.lima@hoymail.com>
2024-11-05 10:07:45 -0300

    101. Registrando o caso de uso SignupUseCase no módulo de usuários

--- src/modules/user/application/usecases/user-delete.usecase.ts
+++ src/modules/user/application/usecases/user-delete.usecase.ts
@@ -1,12 +1,13 @@
 import { DefaultUseCase } from '@/shared/application/usecases/use-case'
 import { UserRepository } from '../../domain/repositories/user-repository'
+import { Injectable } from '@nestjs/common'
 export namespace UserDeleteUseCase {
   export type Request = {
     id: string
   }
 
   export type Response = void
-
+  @Injectable()
   export class UseCase implements DefaultUseCase<Request, Response> {
     constructor(private readonly userRepository: UserRepository.Repository) {}
     async execute(request: Request): Promise<Response> {
--- src/modules/user/infra/user.module.ts
+++ src/modules/user/infra/user.module.ts
@@ -1,11 +1,36 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common'
 
-import { UserService } from "./user.service";
-import { UserCreateController } from "./controllers/user-create.controller";
+import { UserService } from './user.service'
+import { UserCreateController } from './controllers/user-create.controller'
+import { UserSignupUseCase } from '../application/usecases/user-signup.usecase'
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository'
+import { BcryptjsHashProvider } from './providers/hash-provider/bcryptjs-hash.provider'
+import { UserRepository } from '../domain/repositories/user-repository'
+import { HashProvider } from '@/shared/application/providers/hash-provider'
 
 @Module({
-    imports: [],
-    controllers: [UserCreateController],
-    providers: [UserService]
+  imports: [],
+  controllers: [UserCreateController],
+  providers: [
+    UserService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptjsHashProvider,
+    },
+    {
+      provide: UserSignupUseCase.UseCase,
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider,
+      ) => {
+        return new UserSignupUseCase.UseCase(userRepository, hashProvider)
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class UserModule {}
